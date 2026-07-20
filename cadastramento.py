5142cdb34d41d87892f8ac6a5767427b4836095c
Marcos Rigel <marcosrigel1999@gmail.com>
2026-07-20 15:17:31 -0300

    Update cadastramento.py

--- cadastramento.py
+++ cadastramento.py
@@ -1,5 +1,7 @@
 import subprocess
 import time
+import gspread
+import pandas as pd
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -8,11 +10,21 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import SessionNotCreatedException
+from selenium.webdriver.support.ui import Select
 
 class RoboNUPCO:
 
     def __init__(self):
         self.driver = None
+
+    def ler_base_de_dados(self):
+        url = (
+            "https://docs.google.com/spreadsheets/d/"
+            "1aHF6-K3oEeW-H21DKrXGSNJxVPGakRplzH9Og9R4jvo/"
+            "export?format=csv&gid=0"
+        )
+        df = pd.read_csv(url)
+        return df.to_dict(orient="records")
 
     def abrir_aba_efisco(self):
 
@@ -75,12 +87,363 @@ class RoboNUPCO:
 
                 return
 
-    def conectar(self):
+    def etapa_01_cadastramento(self, linha):
 
+        print(f"Iniciando cadastro da OB {linha['Número da OB']}")
+
+        #
+        # Já estamos na tela de Cadastro?
+        #
+        try:
+
+            self.driver.find_element(By.ID, "btt_incluir")
+
+            print("Tela de Cadastro de Prestação de Contas já aberta.")
+
+        except NoSuchElementException:
+
+            print("Abrindo Cadastro de Prestação de Contas...")
+
+            WebDriverWait(self.driver, 60).until(
+                EC.element_to_be_clickable(
+                    (
+                        By.XPATH,
+                        '//*[@id="favoritos_carrossel_itens"]/div/ul/li[1]/a'
+                    )
+                )
+            ).click()
+
+            WebDriverWait(self.driver, 60).until(
+                EC.presence_of_element_located(
+                    (By.ID, "btt_incluir")
+                )
+            )
+
+        #
+        # Novo cadastro
+        #
+        self.driver.find_element(
+            By.ID,
+            "btt_incluir"
+        ).click()
+
+        #
+        # Aguarda primeira tela
+        #
+        WebDriverWait(self.driver, 60).until(
+            EC.element_to_be_clickable(
+                (By.ID, "cdGestao")
+            )
+        )
+
+        #
+        # Unidade de Gestão
+        #
+        Select(
+            WebDriverWait(self.driver, 60).until(
+                EC.element_to_be_clickable(
+                    (By.ID, "cdGestao")
+                )
+            )
+        ).select_by_index(1)
+
+        #
+        # Ano OB
+        #
+        Select(
+            self.driver.find_element(
+                By.ID,
+                "dtAnoExercicioOB"
+            )
+        ).select_by_visible_text("2025")
+
+        #
+        # Processo SEI
+        #
+        self.driver.find_element(
+            By.ID,
+            "nuSei"
+        ).send_keys("0060900077.000814/2025-96")
+
+        #
+        # Observação
+        #
+        self.driver.find_element(
+            By.ID,
+            "txObservacao"
+        ).send_keys("AUXILIO MORADIA COMPETENCIA AGOSTO/2025")
+
+        #
+        # Número da OB
+        #
+        self.driver.find_element(
+            By.ID,
+            "sqOrdemBancaria"
+        ).send_keys(
+            linha["Número da OB"]
+        )
+
+        #
+        # Ano NE
+        #
+        Select(
+            self.driver.find_element(
+                By.ID,
+                "dtAnoExercicioEmpenho"
+            )
+        ).select_by_visible_text("2025")
+
+        #
+        # Número NE
+        #
+        self.driver.find_element(
+            By.ID,
+            "sqEmpenho"
+        ).send_keys(
+            linha["Nota Empenho"]
+        )
+
+        #
+        # Atualiza tela
+        #
+        self.driver.find_element(
+            By.ID,
+            "txObservacao"
+        ).click()
+
+        #
+        # Verifica se já existe
+        #
+        try:
+
+            WebDriverWait(
+                self.driver,
+                1
+            ).until(
+                EC.presence_of_element_located(
+                    (
+                        By.ID,
+                        "btt_prosseguir"
+                    )
+                )
+            )
+
+            self.driver.find_element(
+                By.ID,
+                "btt_prosseguir"
+            ).click()
+
+            print(
+                f"OB {linha['Número da OB']} já cadastrada."
+            )
+
+            WebDriverWait(
+                self.driver,
+                60
+            ).until(
+                EC.presence_of_element_located(
+                    (
+                        By.ID,
+                        "btt_desistir"
+                    )
+                )
+            )
+
+            self.driver.find_element(
+                By.ID,
+                "btt_desistir"
+            ).click()
+
+            return False
+
+        except:
+
+            print("Página 1 preenchida.")
+
+            return True
+
+    def conectar(self):
         options = Options()
         options.debugger_address = "127.0.0.1:9222"
-
         self.driver = webdriver.Chrome(options=options)
+        
+    def etapa_02_continua_cadastro(self, linha):
+
+        print(f"Continuando cadastro da OB {linha['Número da OB']}")
+
+        #
+        # Marca Consulta
+        #
+        WebDriverWait(self.driver, 60).until(
+            EC.element_to_be_clickable(
+                (By.ID, "rdb_consulta")
+            )
+        ).click()
+
+        print("Consulta selecionada.")
+
+        #
+        # Clica em Incluir
+        #
+        WebDriverWait(self.driver, 60).until(
+            EC.element_to_be_clickable(
+                (By.ID, "btt_incluir")
+            )
+        ).click()
+
+        #
+        # Aguarda a segunda tela
+        #
+        WebDriverWait(self.driver, 60).until(
+            EC.presence_of_element_located(
+                (By.ID, "CdTpDocumentoPrestacaoConta")
+            )
+        )
+
+        print("Segunda tela carregada.")
+
+        # ==================================================
+        # SEGUNDA TELA
+        # ==================================================
+
+        #
+        # Tipo do Documento
+        #
+        Select(
+            self.driver.find_element(
+                By.ID,
+                "CdTpDocumentoPrestacaoConta"
+            )
+        ).select_by_visible_text(
+            "78 - D - RELAÇÃO EXTERNA CARIMBADA P/ BANCO"
+        )
+
+        #
+        # Data
+        #
+        self.driver.find_element(
+            By.ID,
+            "dtDocPc"
+        ).send_keys(
+            str(linha["Data da OB"]).zfill(8)
+        )
+
+        #
+        # Número da Remessa
+        #
+        self.driver.find_element(
+            By.ID,
+            "nuIdentificacao"
+        ).send_keys(
+            linha["Número da Remessa"]
+        )
+
+        #
+        # Descrição
+        #
+        self.driver.find_element(
+            By.ID,
+            "txDescricao"
+        ).send_keys(
+            "AUXILIO MORADIA COMPETENCIA AGOSTO/2025"
+        )
+
+        #
+        # Referência SEI
+        #
+        self.driver.find_element(
+            By.ID,
+            "txReferenciaPagina"
+        ).send_keys(
+            "0060900077.000814/2025-96"
+        )
+
+        #
+        # Item do Empenho
+        #
+        self.driver.find_element(
+            By.ID,
+            "rdb_itemEmpenho[[*]]0"
+        ).click()
+
+        print("Página 2 preenchida.")
+
+        # ==================================================
+        # 11) CONFIRMA
+        # ==================================================
+
+        WebDriverWait(
+            self.driver,
+            60
+        ).until(
+            EC.element_to_be_clickable(
+                (By.ID, "btt_confirmar")
+            )
+        ).click()
+
+        print("Primeira confirmação realizada.")
+
+        # ==================================================
+        # 12) TERCEIRA TELA
+        # ==================================================
+
+        WebDriverWait(
+            self.driver,
+            60
+        ).until(
+            EC.presence_of_element_located(
+                (By.ID, "btt_incluir")
+            )
+        )
+
+        WebDriverWait(
+            self.driver,
+            60
+        ).until(
+            EC.element_to_be_clickable(
+                (By.ID, "btt_confirmar")
+            )
+        ).click()
+
+        print("Segunda confirmação realizada.")
+
+        return True
+
+    def etapa_03_finalizar_cadastro(self, linha):
+
+        print("Finalizando cadastro...")
+
+        #
+        # Aguarda a quarta tela
+        #
+        WebDriverWait(
+            self.driver,
+            60
+        ).until(
+            EC.element_to_be_clickable(
+                (
+                    By.ID,
+                    "btt_prosseguir"
+                )
+            )
+        )
+
+        #
+        # Prosseguir
+        #
+        self.driver.find_element(
+            By.ID,
+            "btt_prosseguir"
+        ).click()
+
+        print(
+            f"✅ Cadastro da OB {linha['Número da OB']} concluído."
+        )
+
+        print("Aguardando próximo registro da planilha...")
+
+        return True
+
 
     def tratar_tela_ja_logado(self):
 
@@ -104,10 +467,43 @@ class RoboNUPCO:
 
     def iniciar(self):
 
+        print("Conectando ao Chrome...")
         self.conectar()
+
+        print("Abrindo eFisco...")
         self.abrir_aba_efisco()
-        status = self.aguardar_login_efisco()
-        print(status)
+
+        print("Verificando login...")
+        self.aguardar_login_efisco()
+
+        print("Lendo Google Sheets...")
+        dados = self.ler_base_de_dados()
+
+        print(f"{len(dados)} registros encontrados.")
+
+        for linha in dados[:20]:
+
+            print()
+            print("=" * 60)
+            print(f"OB {linha['Número da OB']}")
+            print("=" * 60)
+
+            #
+            # Etapa 01
+            #
+            if not self.etapa_01_cadastramento(linha):
+                continue
+
+            #
+            # Etapa 02
+            #
+            if not self.etapa_02_continua_cadastro(linha):
+                continue
+
+            #
+            # Etapa 03
+            #
+            self.etapa_03_finalizar_cadastro(linha)
 
 
 if __name__ == "__main__":
